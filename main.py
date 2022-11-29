6e6f76927b2941e497e7a49bae6825b59dea08f9
gabriel feijó vilela <61162949+vilelagabrielx@users.noreply.github.com>
2022-11-29 17:22:47 -0300

    organização do código.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ eel.init("web")
 def tentativa_login(login: str,senha : str):
     try:
 
-        trylogin = requests.get(f'http://192.168.3.18:5000/get/getlogin?login={login}&senha={senha}')
+        trylogin = requests.get(f'http://192.168.0.150:5000/get/getlogin?login={login}&senha={senha}')
         
         responselogin = trylogin.content.decode('utf-8')
       
@@ -59,28 +59,36 @@ def tentativa_login(login: str,senha : str):
 
 # Start the index.html file
 try:
-    with open('session.json', 'r') as s:
+    with open("config.json",'r') as r:
 
-        session_json = json.load(s)
-        
-        login = session_json["login"]  # type: ignore
-        
-        senha = session_json["senha"]  # type: ignore
-        
-        logindec = cripto.decriptar(bytes(login.encode('utf-8'))).decode('utf-8')
-        
-        senhadec = cripto.decriptar(bytes(senha.encode('utf-8'))).decode('utf-8')  
-        
-        trylogin = requests.get(f'http://192.168.3.18:5000/get/getlogin?login={logindec}&senha={senhadec}')
-        
-        
-        if trylogin.content.decode('utf-8') != 'ACEITO':
-        
-            eel.start("index.html",disable_cache=True)
-        
-        else:
-        
-            eel.start("session_check.html",disable_cache=True)
+        config = json.load(r)
+
+        iplocal = config["iplocal"]
+
+        porta = config["porta"]
+
+        with open('session.json', 'r') as s:
+
+            session_json = json.load(s)
+            
+            login = session_json["login"]  # type: ignore
+            
+            senha = session_json["senha"]  # type: ignore
+            
+            logindec = cripto.decriptar(bytes(login.encode('utf-8'))).decode('utf-8')
+            
+            senhadec = cripto.decriptar(bytes(senha.encode('utf-8'))).decode('utf-8')  
+            
+            trylogin = requests.get(f'http://{iplocal}:{porta}/get/getlogin?login={logindec}&senha={senhadec}')
+            
+            
+            if trylogin.content.decode('utf-8') != 'ACEITO':
+            
+                eel.start("index.html",disable_cache=True)
+            
+            else:
+            
+                eel.start("session_check.html",disable_cache=True)
 except:
     
     eel.start("index.html",disable_cache=True)
